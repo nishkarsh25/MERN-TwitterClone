e55d01af5e47d784720081bdf54cfdce33297fc5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:29:43 +0530

    Imports axios import:
    import axios from "axios";
    Imports Axios, a popular library for making HTTP requests from the browser and Node.js.
    Constant import:
    
    import { TWEET_API_END_POINT } from "../utils/constant";
    Imports the constant TWEET_API_END_POINT from a file located at "../utils/constant". This constant likely holds the base URL for the tweet-related API endpoints.
    React hooks imports:
    
    import { useEffect } from "react";
    import { useDispatch, useSelector } from "react-redux";
    Imports React hooks: useEffect for handling side effects in functional components, useDispatch to dispatch actions to Redux store, and useSelector to extract data from the Redux store.
    Redux action import:
    
    import { getAllTweets } from "../redux/tweetSlice";
    Imports the getAllTweets action creator from the tweetSlice slice of the Redux store. This action is used to update the state with fetched tweets.

---
+++ frontend/twitterclone/src/hooks/useGetMyTweets.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { TWEET_API_END_POINT } from "../utils/constant";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTweets } from "../redux/tweetSlice";
+
+
