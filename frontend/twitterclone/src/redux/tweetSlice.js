90dd101c62947e9d1d804e83473f1d150dcb83b3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:21:20 +0530

    Initial State:
    initialState: { tweets: null, refresh: false, isActive: true },
    Defines the initial state of the slice with three properties:
    tweets: Initially null, this will hold an array of tweets or null.
    refresh: Initially false, used as a boolean flag to trigger a refresh or re-rendering of tweets.
    isActive: Initially true, indicates the active state of the component or feature related to tweets.

--- frontend/twitterclone/src/redux/tweetSlice.js
+++ frontend/twitterclone/src/redux/tweetSlice.js
@@ -1,5 +1,10 @@
 import {createSlice} from "@reduxjs/toolkit";
 const tweetSlice = createSlice({
     name:"tweet",
+    initialState:{
+        tweets:null,
+        refresh:false,
+        isActive:true
+    },
     
 });
