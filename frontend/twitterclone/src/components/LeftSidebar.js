64af9a1eb88bc3ef392d219d3ae28bd8940b90d8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:03:07 +0530

    Imports React Icons:
    Imports various icons (CiHome, CiHashtag, IoIosNotificationsOutline, CiUser, CiBookmark, AiOutlineLogout) from the respective packages (react-icons/ci and react-icons/ai) to use as graphical elements in the sidebar.
    React Router Dom:
    
    Imports Link and useNavigate for navigation within the React application.
    React Redux:
    
    Imports useSelector and useDispatch hooks from React Redux to access and dispatch actions to the Redux store.
    Axios and Constants:
    
    Imports axios for making HTTP requests to the backend API and USER_API_END_POINT from ../utils/constant for storing the API endpoint related to user actions.
    React Hot Toast:
    
    Imports toast from react-hot-toast for displaying toast notifications.
    Redux Actions:
    
    Imports getMyProfile, getOtherUsers, and getUser actions from ../redux/userSlice to update Redux store state related to user data.

---
+++ frontend/twitterclone/src/components/LeftSidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CiHome } from "react-icons/ci";
+import { CiHashtag } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CiUser } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+import { AiOutlineLogout } from "react-icons/ai";
+import { Link,useNavigate } from 'react-router-dom';
+import {useSelector,useDispatch} from "react-redux";
+import axios from "axios";
+import { USER_API_END_POINT } from '../utils/constant';
+import toast from "react-hot-toast"
+import { getMyProfile, getOtherUsers, getUser } from '../redux/userSlice';
+ 
+
+
