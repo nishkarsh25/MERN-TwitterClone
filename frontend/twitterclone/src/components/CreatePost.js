8728ee5907c76feadf7403c04d63dee3c4ffe17b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:11:09 +0530

    Imports React and Hooks:
    Imports React and useState from React for creating stateful components.
    UI Components and Icons:
    
    Imports Avatar component from react-avatar for displaying user avatars.
    Imports CiImageOn icon from react-icons/ci for an image icon.
    Network Requests and Constants:
    
    Imports axios for making HTTP requests.
    Imports TWEET_API_END_POINT from '../utils/constant' for the API endpoint related to tweets.
    Imports toast from react-hot-toast for displaying notifications.
    Redux Hooks and Actions:
    
    Imports useSelector and useDispatch from react-redux for accessing the Redux store and dispatching actions.
    Imports getAllTweets, getIsActive, and getRefresh from '../redux/tweetSlice' for updating tweet state in Redux.

---
+++ frontend/twitterclone/src/components/CreatePost.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Avatar from "react-avatar";
+import { CiImageOn } from "react-icons/ci";
+import axios from "axios";
+import { TWEET_API_END_POINT } from "../utils/constant";
+import toast from "react-hot-toast"
+import { useSelector, useDispatch } from "react-redux";
+import { getAllTweets, getIsActive, getRefresh } from '../redux/tweetSlice';
+
+
+
