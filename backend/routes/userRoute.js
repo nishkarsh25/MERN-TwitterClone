7663f6f201f5b117b0706a89b1554875480b3a46
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:06:45 +0530

    Router Setup Creating the Router: An Express router is created using express.Router(). The router allows us to define routes and their handlers in a modular way.

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { Login, Register, bookmark, follow, getMyProfile, getOtherUsers, logout, unfollow } from "../controllers/userController.js";
 import isAuthenticated from "../config/auth.js";
 
-
+const router = express.Router();
 
 
 
