d44c4dc58fdf578f2ef1884f473dde288d5f55ff
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 11:59:22 +0530

    Handling the Promise from mongoose.connect():
    The mongoose.connect() function returns a promise, so .then() and .catch() methods are used to handle the success and error cases, respectively.
    In the .then() block, a callback function logs "Connected to mongoDB" to the console if the connection is successful.
    In the .catch() block, a callback function logs the error to the console if the connection fails. This helps with debugging by providing immediate feedback on why the connection failed.

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,8 +5,8 @@ dotenv.config({
 })
 const databaseConnection = () => {
     mongoose.connect(process.env.MONGO_URI).then(()=>{
-        
+        console.log("Connected to mongoDB");
     }).catch((error)=>{
-        
+        console.log(error);
     })
 }
