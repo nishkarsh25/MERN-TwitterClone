a92cd9ad2c7b830600e417a33560b6ce373a448b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:06:37 +0530

    Imports Importing express:
    The express library is imported to create the router. Express is a web application framework for Node.js, providing robust features for building web and mobile applications.
    Importing Controllers:
    
    Various controller functions are imported from the userController.js file. These functions handle different user-related operations such as login, registration, bookmarking, following, etc.
    Importing isAuthenticated Middleware:
    
    The isAuthenticated middleware is imported from the auth.js file. This middleware is used to protect routes, ensuring that only authenticated users can access them.

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { Login, Register, bookmark, follow, getMyProfile, getOtherUsers, logout, unfollow } from "../controllers/userController.js";
+import isAuthenticated from "../config/auth.js";
+
+
+
+
+
