34ac64f666fa46286a597aec7fa1b43c4f9f6c51
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:09:11 +0530

    Functional Component Feed Redux State (useSelector(store => store.tweet)):
    Uses useSelector to extract the tweets array from the Redux store's tweet slice.

--- frontend/twitterclone/src/components/Feed.js
+++ frontend/twitterclone/src/components/Feed.js
@@ -3,5 +3,8 @@ import CreatePost from './CreatePost.js'
 import Tweet from './Tweet.js'
 import {useSelector} from "react-redux";
 
-
+const Feed = () => {
+  const {tweets} = useSelector(store=>store.tweet);
+  
+}
 
