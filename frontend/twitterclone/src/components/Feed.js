87415339c67c688126c496a6079ebd53733b634f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:09:44 +0530

    Child Components:
    Renders the CreatePost component, which is presumably used to create new tweets/posts.
    Maps through the tweets array using tweets?.map(), where each tweet is passed to the Tweet component as a prop (tweet={tweet}).
    Uses the key prop with tweet?._id to ensure each rendered Tweet component has a unique identifier.

--- frontend/twitterclone/src/components/Feed.js
+++ frontend/twitterclone/src/components/Feed.js
@@ -7,7 +7,14 @@ const Feed = () => {
   const {tweets} = useSelector(store=>store.tweet);
   return (
     <div className='w-[50%] border border-gray-200'>
-      
+      <div>
+        <CreatePost/>
+        {
+          tweets?.map((tweet)=> <Tweet key={tweet?._id} tweet={tweet}/>)
+        }
+        
+         
+      </div>
     </div>
   )
 }
