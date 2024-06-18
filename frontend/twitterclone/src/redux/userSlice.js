eedbfb6871b055b2e5685f20b9b4f9f4f2113263
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:19:37 +0530

    Slice Configuration Name:
    name: "user",
    This sets the name of the slice to "user". This name will be used as a prefix for the action types generated by createSlice.
    Initial State:
    
    initialState: { user: null, otherUsers: null, profile: null },
    This defines the initial state of the slice. The initial state includes three properties:
    user: Initially null, this will hold the current user's data.
    otherUsers: Initially null, this will hold data of other users.
    profile: Initially null, this will hold the profile data of the user.

--- frontend/twitterclone/src/redux/userSlice.js
+++ frontend/twitterclone/src/redux/userSlice.js
@@ -1,8 +1,12 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    
-    
+    name:"user",
+    initialState:{
+        user:null,
+        otherUsers:null,
+        profile:null
+    },
     
 });
 
