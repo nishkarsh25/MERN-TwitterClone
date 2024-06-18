d4eee806b97f2ac5a664c82b9baf1da8db2310a6
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 11:59:06 +0530

    Defining the databaseConnection Function:
    A function named databaseConnection is defined. This function is responsible for establishing a connection to the MongoDB database.
    Connecting to MongoDB:
    
    Inside the databaseConnection function, mongoose.connect() is called with process.env.MONGO_URI as its argument. process.env.MONGO_URI is expected to contain the MongoDB connection string, which should have been loaded from the .env file. This connection string includes the protocol (mongodb:// or mongodb+srv://), username, password, host, and database name.

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,4 +3,10 @@ import dotenv from "dotenv";
 dotenv.config({
     path:"../config/.env"
 })
-
+const databaseConnection = () => {
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        
+    }).catch((error)=>{
+        
+    })
+}
