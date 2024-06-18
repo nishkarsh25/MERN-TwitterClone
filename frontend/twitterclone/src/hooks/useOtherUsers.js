ff1d2910dd38699464971c4c1502e06ee46453a1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:33:03 +0530

    useEffect hook:
    useEffect(() => { ... }, []);
    Executes the callback function once after the component mounts.
    Inside the useEffect callback:
    Defines an asynchronous function fetchOtherUsers using async and await to handle the Axios GET request.
    Uses Axios to fetch other users' data from ${USER_API_END_POINT}/otheruser/${id} with withCredentials: true option, indicating credentials should be sent with the request.
    Logs the response res to the console for debugging purposes.
    Dispatches the getOtherUsers action with res.data.otherUsers as the payload. This action updates the Redux state with the fetched other users' data.
    Catches and logs any errors that occur during the Axios request.

--- frontend/twitterclone/src/hooks/useOtherUsers.js
+++ frontend/twitterclone/src/hooks/useOtherUsers.js
@@ -6,5 +6,18 @@ import { getMyProfile, getOtherUsers } from "../redux/userSlice";
 
 const useOtherUsers = (id) => {
     const dispatch = useDispatch();
-    
+    useEffect(()=>{
+        const fetchOtherUsers = async () => {
+            try {
+                const res = await axios.get(`${USER_API_END_POINT}/otheruser/${id}`,{
+                    withCredentials:true
+                });
+                console.log(res);
+                dispatch(getOtherUsers(res.data.otherUsers));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers();
+    },[]);
 };
