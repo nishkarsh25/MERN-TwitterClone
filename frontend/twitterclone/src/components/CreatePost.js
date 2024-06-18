3a1a366bc155ebd806b4072b1b2f2efb298e5519
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:11:59 +0530

    Event Handlers (submitHandler, forYouHandler, followingHandler):
    submitHandler: Handles the submission of a new tweet/post.
    Sends a POST request to the server using axios.post to create a new tweet with description and id (user ID).
    Dispatches getRefresh() to update the tweet list after a new post is created.
    Shows a success toast notification if the creation is successful; otherwise, shows an error toast.
    Resets description to an empty string after submission.
    forYouHandler and followingHandler: Toggle between "For You" and "Following" tabs.
    Dispatch getIsActive(true) or getIsActive(false) to update isActive state in Redux based on the tab clicked.

--- frontend/twitterclone/src/components/CreatePost.js
+++ frontend/twitterclone/src/components/CreatePost.js
@@ -13,10 +13,33 @@ const CreatePost = () => {
     const {isActive} = useSelector(store=>store.tweet);
     const dispatch = useDispatch();
 
-    
+    const submitHandler = async () => {
 
-    
-    
+        try {
+            const res = await axios.post(`${TWEET_API_END_POINT}/create`, { description, id: user?._id }, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true
+            });
+            dispatch(getRefresh());
+            if (res.data.success) {
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log(error);
+        }
+        setDescription("");
+    }
+
+    const forYouHandler = () => {
+         dispatch(getIsActive(true));
+    }
+    const followingHandler = () => {
+        
+        dispatch(getIsActive(false));
+    }
 
     
 }
