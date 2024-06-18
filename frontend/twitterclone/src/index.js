af59e5377ee43803f64178b858b719afe8c8ffe4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:24:23 +0530

    Imports React and ReactDOM:
    React: The core library for building UI components in React.
    ReactDOM: Provides DOM-specific methods that can be used to render React elements into the DOM.
    react-redux imports:
    
    Provider: A React component from react-redux that provides the Redux store to its child components.
    Redux-related imports:
    
    store: Imports the Redux store created with configureStore from the redux setup.
    PersistGate: A component from redux-persist/integration/react that delays rendering of the app's UI until persisted state has been retrieved and saved to redux.
    redux-persist imports:
    
    persistStore: A function from redux-persist used to create a persistor object to manage state persistence.
    Store Configuration
    persistor setup:
    let persistor = persistStore(store);
    Creates a persistor by invoking persistStore with the Redux store. The persistor is responsible for managing the state persistence.

---
+++ frontend/twitterclone/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from "react-redux";
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistStore} from "redux-persist";
+
+
+
+
+
+
