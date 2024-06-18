5db7f7f6b134305f8df687acf39a3b11b66dfd31
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:13:33 +0530

    Route Definitions Create Tweet Route:
    router.route("/create").post(isAuthenticated, createTweet);
    Method: POST
    Path: /create
    Middleware: isAuthenticated to ensure the user is authenticated before creating a tweet.
    Handler: Calls the createTweet function from the controller to handle the creation of a new tweet.
    Delete Tweet Route:
    
    router.route("/delete/:id").delete(isAuthenticated, deleteTweet);
    Method: DELETE
    Path: /delete/:id
    Middleware: isAuthenticated to ensure the user is authenticated before deleting a tweet.
    Handler: Calls the deleteTweet function, passing the tweet ID from the request parameters to handle tweet deletion.
    Like or Dislike Tweet Route:
    
    router.route("/like/:id").put(isAuthenticated, likeOrDislike);
    Method: PUT
    Path: /like/:id
    Middleware: isAuthenticated to ensure the user is authenticated before liking or disliking a tweet.
    Handler: Calls the likeOrDislike function, passing the tweet ID from the request parameters to handle liking or disliking a tweet.
    Get All Tweets Route:
    
    router.route("/alltweets/:id").get(isAuthenticated, getAllTweets);
    Method: GET
    Path: /alltweets/:id
    Middleware: isAuthenticated to ensure the user is authenticated before fetching all tweets.
    Handler: Calls the getAllTweets function, passing the user ID from the request parameters to fetch all tweets from the logged-in user and the users they are following.
    Get Following Tweets Route:
    
    router.route("/followingtweets/:id").get(isAuthenticated, getFollowingTweets);
    Method: GET
    Path: /followingtweets/:id
    Middleware: isAuthenticated to ensure the user is authenticated before fetching tweets from users they are following.
    Handler: Calls the getFollowingTweets function, passing the user ID from the request parameters to fetch tweets from users the logged-in user is following.
    Exporting the Router
    Exporting the Router:
    export default router;
    The router instance is exported as the default export, making it available for import in other parts of the application, such as the main server file.

--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -4,4 +4,9 @@ import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
  
-
+router.route("/create").post(isAuthenticated,createTweet);
+router.route("/create").post(isAuthenticated,createTweet);
+router.route("/delete/:id").delete(isAuthenticated,deleteTweet);
+router.route("/like/:id").put(isAuthenticated,likeOrDislike);
+router.route("/alltweets/:id").get(isAuthenticated, getAllTweets);
+router.route("/followingtweets/:id").get(isAuthenticated, getFollowingTweets);
