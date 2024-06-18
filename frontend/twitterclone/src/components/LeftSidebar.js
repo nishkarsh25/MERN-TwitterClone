f516ad264b940898d54ae71e545892c43673d1c2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:03:37 +0530

    Logout Handler (logoutHandler function):
    Async Function for Logout:
    Defines an asynchronous function logoutHandler responsible for logging out the user.
    Uses axios to send a GET request to ${USER_API_END_POINT}/logout for logging out the user session on the server.
    Dispatches Redux actions (getUser(null), getOtherUsers(null), getMyProfile(null)) to clear user-related data from the Redux store.
    Uses navigate('/login') to redirect the user to the login page after successful logout.
    Displays a success toast notification (toast.success(res.data.message)) upon successful logout.
    Catches and logs any errors (console.log(error)) that occur during the logout process.

--- frontend/twitterclone/src/components/LeftSidebar.js
+++ frontend/twitterclone/src/components/LeftSidebar.js
@@ -18,7 +18,18 @@ const LeftSidebar = () => {
     const {user} = useSelector(store=>store.user);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    
+    const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${USER_API_END_POINT}/logout`);
+            dispatch(getUser(null));
+            dispatch(getOtherUsers(null));
+            dispatch(getMyProfile(null));
+            navigate('/login');
+            toast.success(res.data.message);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     
 }
