6c178294ccb3809bfa9eb22171b956a8302748ee
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:35:20 +0530

    Imports React import:
    import React from 'react';
    Imports the React library, allowing the use of JSX and React components.
    Router imports:
    
    import { createBrowserRouter, RouterProvider } from "react-router-dom";
    Imports createBrowserRouter and RouterProvider from react-router-dom. These are utilities used to configure and provide routing capabilities within the application.
    Component imports:
    
    import Login from './Login';
    import Home from './Home';
    import Feed from './Feed';
    import Profile from './Profile';
    Imports components (Login, Home, Feed, Profile) that will be rendered based on the defined routes.

---
+++ frontend/twitterclone/src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './Login';
+import Home from './Home';
+import Feed from './Feed';
+import Profile from './Profile';
+
+
+
