f3dfb4587edb32887067263ebdb7b01feb74d412
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:13:21 +0530

    Router Setup Creating the Router: const router = express.Router(); An instance of the Express router is created. This allows the definition of various routes and their handlers.

--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -2,6 +2,6 @@ import express from "express";
 import { createTweet, deleteTweet, getAllTweets, getFollowingTweets, likeOrDislike } from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
-
+const router = express.Router();
  
 
