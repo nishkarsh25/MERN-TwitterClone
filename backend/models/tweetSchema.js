46c0d4e9624a976ff2b7ade8c39d20e1c111c158
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:09:31 +0530

    Schema Definition Defining the tweetSchema:
    A new schema named tweetSchema is created using new mongoose.Schema(). This schema defines the structure of documents within the Tweet collection in the database.
    Description Field:
    
    description Field:
    Type: String
    Required: true
    Purpose: Stores the textual content of the tweet. This field is mandatory, meaning each tweet must have a description.
    Like Field:
    
    like Field:
    Type: Array
    Default: []
    Purpose: Stores an array of user IDs who liked the tweet. By default, this array is empty.
    User ID Field:
    
    userId Field:
    Type: mongoose.Schema.Types.ObjectId
    Reference: "User"
    Purpose: Stores the ID of the user who created the tweet. It references the User model, indicating a relationship between tweets and users.
    User Details Field:
    
    userDetails Field:
    Type: Array
    Default: []
    Purpose: Stores additional details about the user who created the tweet. By default, this array is empty. This field can hold user-related information such as the user's name, username, profile picture, etc.

--- backend/models/tweetSchema.js
+++ backend/models/tweetSchema.js
@@ -1,3 +1,20 @@
 import mongoose from "mongoose";
 
-const tweetSchema = new mongoose.Schema();
+const tweetSchema = new mongoose.Schema({
+    description:{
+        type:String,
+        required:true
+    },
+    like:{
+        type:Array,
+        default:[]
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    userDetails:{
+        type:Array,
+        default:[]
+    },
+});
