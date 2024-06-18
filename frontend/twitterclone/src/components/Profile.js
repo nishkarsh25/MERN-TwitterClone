19c7af570b882834cc9226edd3b38ff3e28110c0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:06:55 +0530

    JSX Rendering:
    Renders JSX elements to display the profile page layout.
    Displays the user's banner image, profile avatar, profile name, username, bio, and buttons (Edit Profile or Follow/Following) based on the relationship between the current user and the profile user.

--- frontend/twitterclone/src/components/Profile.js
+++ frontend/twitterclone/src/components/Profile.js
@@ -47,6 +47,41 @@ const Profile = () => {
         }
     }
 
-    
+    return (
+        <div className='w-[50%] border-l border-r border-gray-200'>
+            <div>
+                <div className='flex items-center py-2'>
+                    <Link to="/" className='p-2 rounded-full hover:bg-gray-100 hover:cursor-pointer'>
+                        <IoMdArrowBack size="24px" />
+                    </Link>
+                    <div className='ml-2'>
+                        <h1 className='font-bold text-lg'>{profile?.name}</h1>
+                        <p className='text-gray-500 text-sm'>10 post</p>
+                    </div>
+                </div>
+                <img src="https://pbs.twimg.com/profile_banners/1581707412922200067/1693248932/1080x360" alt="banner" />
+                <div className='absolute top-52 ml-2 border-4 border-white rounded-full'>
+                    <Avatar src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg" size="120" round={true} />
+                </div>
+                <div className='text-right m-4'>
+                    {
+                        profile?._id === user?._id ? (
+                            <button className='px-4 py-1 hover:bg-gray-200 rounded-full border border-gray-400'>Edit Profile</button>
+
+                        ) : (
+                            <button onClick={followAndUnfollowHandler} className='px-4 py-1 bg-black text-white rounded-full'>{user.following.includes(id) ? "Following" : "Follow"}</button>
+                        )
+                    }
+                </div>
+                <div className='m-4'>
+                    <h1 className='font-bold text-xl'>{profile?.name}</h1>
+                    <p>{`@${profile?.username}`}</p>
+                </div>
+                <div className='m-4 text-sm'>
+                    <p>🌐 Exploring the web's endless possibilities with MERN Stack 🚀 | Problem solver by day, coder by night 🌙 | Coffee lover ☕ | Join me on this coding journey!</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
