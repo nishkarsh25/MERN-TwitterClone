fc2bd4fe8f23d5102ddf43c9f81851c27a05f1eb
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 13:00:36 +0530

    Imports React and Hooks:
    import React, { useEffect } from 'react';
    Imports the main React library and the useEffect hook for handling side effects in functional components.
    React Router Dom:
    
    import { Outlet, useNavigate } from "react-router-dom";
    Imports Outlet component and useNavigate hook from React Router Dom.
    Outlet: Placeholder for nested route content.
    useNavigate: Hook for navigation between routes programmatically.
    Redux:
    
    import { useSelector } from "react-redux";
    Imports useSelector hook from React Redux for accessing Redux store state.
    Custom Hooks:
    
    import useOtherUsers from '../hooks/useOtherUsers';
    import useGetMyTweets from '../hooks/useGetMyTweets';
    Imports custom hooks (useOtherUsers and useGetMyTweets) for fetching other users and user's tweets respectively.

---
+++ frontend/twitterclone/src/components/Home.js
@@ -0,0 +1,11 @@
+import React,{useEffect} from 'react'
+import LeftSidebar from './LeftSidebar'
+import RightSidebar from './RightSidebar'
+import { Outlet, useNavigate } from "react-router-dom";
+import useOtherUsers from '../hooks/useOtherUsers';
+import { useSelector } from "react-redux";
+import useGetMyTweets from '../hooks/useGetMyTweets';
+
+
+
+
