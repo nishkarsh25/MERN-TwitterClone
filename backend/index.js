99a38ebb106277852230cba76d67677a07c88c54
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-18 12:16:36 +0530

    Imports Importing Libraries and Modules: express: A web framework for Node.js used to create the server and define routes. dotenv: A module to load environment variables from a .env file into process.env. databaseConnection: A custom module that establishes a connection to the MongoDB database. cookieParser: A middleware to parse cookies attached to the client request object. userRoute and tweetRoute: Custom route modules handling user-related and tweet-related routes, respectively. cors: A middleware to enable Cross-Origin Resource Sharing (CORS), allowing the server to handle requests from different origins.

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import databaseConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
+
+
+
+
+
+
+ 
+
